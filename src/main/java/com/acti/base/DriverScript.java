9393cc5fd651acd33d3fe87ef30f306c736eda83
MuthuMalai <MuthuMalai@LAPTOP-UR72A20M>
2020-11-06 14:19:05 -0500

    commiting DriverScript code

--- src/main/java/com/acti/base/DriverScript.java
+++ src/main/java/com/acti/base/DriverScript.java
@@ -1,5 +1,57 @@
 package com.acti.base;
 
-public class DriverScript {
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class DriverScript {
+	
+	public static Properties prop;
+	public static WebDriver driver;
+	public DriverScript(){
+		try{
+		File file=new File("./src/test/resources/config/config.properties");
+		FileInputStream fis=new FileInputStream(file);
+		prop=new Properties();
+		prop.load(fis);
+		}
+		catch(Exception e){
+			System.out.println("Unable to load file"+e.getMessage());
+		}
+	}
+	@Test
+	public static void initBrowser() throws Exception{
+		//the data in config file is stored as key-value pair. If we pass the key to getProperty() method it will return the value
+		String browser=prop.getProperty("browser");
+		if(browser.equalsIgnoreCase("chrome")){
+			System.setProperty("webdriver.chrome.driver", "./src/test/resources/browsers/chromedriver.exe");
+			driver=new ChromeDriver();
+		}
+		else if(browser.equalsIgnoreCase("firefox")){
+			System.setProperty("webdriver.gecko.driver", "./src/test/resources/browsers/geckodriver.exe");
+			driver=new ChromeDriver();
+		}
+		else{
+			System.out.println("Unsupported browser name");
+		}
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(3, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		launchApplication();
+		Thread.sleep(2000);
+		driver.close();
+		
+	}
+	public static void launchApplication(){
+		driver.get(prop.getProperty("qaurl"));
+	}
+	public static void closeApplication(){
+		driver.close();
+	}
 }
+
