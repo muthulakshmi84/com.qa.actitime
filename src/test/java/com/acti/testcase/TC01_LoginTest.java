8c553ea01315f436223fb75bb5b7b6f829d41379
MuthuMalai <MuthuMalai@LAPTOP-UR72A20M>
2020-11-17 10:08:07 -0500

    Updating and commiting LoginTest with dataprovider

--- src/test/java/com/acti/testcase/TC01_LoginTest.java
+++ src/test/java/com/acti/testcase/TC01_LoginTest.java
@@ -1,19 +1,41 @@
 package com.acti.testcase;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import com.acti.utils.ExcelLib;
+import com.acti.utils.Generic;
 
 public class TC01_LoginTest extends BaseTest {
 	
 	@Test
-	public void TestLoginFunction() throws Exception{
+	public void TestLoginFunction(){
 		loginPage.loginToApplication("admin", "manager");
-		Thread.sleep(3000);
+		Generic.fn_sleep();
 	}
 	
 	@Test
-	public void testVerifyLogo() throws Exception{
+	public void testVerifyLogo(){
 		loginPage.verifyActiLogo();
-		Thread.sleep(3000);
+		Generic.fn_sleep();
+	}
+	
+	@Test(dataProvider="actiData")
+	public void testLoginWithMultipleUsers(String username, String password){
+		loginPage.loginToApplication("admin", "manager");
+		Generic.fn_sleep();
+	}
+	
+	@DataProvider(name="actiData")
+	public Object[][] testData(){
+		ExcelLib xl=new ExcelLib();
+		int row=xl.getRowCount(0);
+		Object[][] data=new Object[row][2];
+		for(int i=0; i<row; i++){
+			data[i][0]=xl.getData(0, i, 0);
+			data[i][1]=xl.getData(0, i, 1);
+		}
+		return data;		
 	}
 	
 
